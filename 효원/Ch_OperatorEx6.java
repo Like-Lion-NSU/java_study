903f8300c97d225ab5061a1ed6998f4d9deb0d1b
hyowon6 <hyowonng@gmail.com>
2023-03-31 01:21:12 +0900

    [Pre]Ch3_OperatorEx6

---
+++ 효원/Ch_OperatorEx6.java
@@ -0,0 +1,9 @@
+public class Ch_OperatorEx6 {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 20;
+        byte c = a + b;// 명시적으로 형변환이 필요
+        //byte c = (byte)(a+b);
+        System.out.prinln(c);
+    }
+}
