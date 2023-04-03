6904d1f67fa64c132cea26ace9881fb6cf87f39e
hyowon6 <hyowonng@gmail.com>
2023-04-04 00:52:05 +0900

    [pre]Ch04_FlowEx13

---
+++ Exercise/Exercise3_10.java
@@ -0,0 +1,22 @@
+package Exercise;
+
+// 대문자를 소문자로 변경하는 코드, 문자 ch에 저장된 문자가 대문자인 경우에만 소문자로 변경한다.
+// 문자코드는 소문자가 대문자보다 32만큼 더 크다.
+// 예를 들어, 'A'의 코드는 65이고, 'a'의 코드는 97이다.
+// (1) (2)에 알맞는 코드를 넣으시오.
+
+public class Exercise3_10 {
+    public static void main(String[] args) {
+        char ch = 'A';
+
+        //char lowerCase = (1) ? (2) : ch;
+        char lowerCase = (ch == 65) ? 'a' : ch;
+        //lowerCase에 'a'가 들어가기 위해서는 조건식이 참이어야함
+        //문자 ch에 저장된 문자가 대문자인 경우에만 소문자로 변경하기 때문에
+        //ch의 코드를 활용하여 ch가 대문자인지 확인하는 조건식을 작성함
+
+
+        System.out.println("ch:"+ch);
+        System.out.println("ch to lowerCase:"+lowerCase);
+    }
+}
---
+++ Exercise/Exercise3_2.java
@@ -0,0 +1,60 @@
+package Exercise;
+
+import java.util.Scanner;
+
+public class Exercise3_2 {
+    public static void main(String[] args) {
+
+//          for문 이용
+//        Scanner scanner = new Scanner(System.in);//Scanner클래스의 객체를 생성
+//        System.out.print("국어: ");
+//        int korean = scanner.nextInt();//입력받은 내용을 korean에 저장
+//        System.out.print("영어: ");
+//        int english = scanner.nextInt();
+//        System.out.print("수학: ");
+//        int math = scanner.nextInt();
+//
+//        int avr = (korean + english + math) / 3;
+        //평균값 저장
+//
+//        if (avr >= 90) {// 평균이 90점 이상이면
+//            System.out.println("A");// A 출력
+//        } else if (avr >= 80) {
+//            System.out.println("B");
+//        } else if (avr >= 70) {
+//            System.out.println("C");
+//        } else if (avr >= 60) {
+//            System.out.println("D");
+//        } else {//어느 조건식도 만족하지 않았으면
+//            System.out.println("불합격입니다.");//불합격입니다 출력
+//        }
+        //Switch를 이용
+        Scanner scanner = new Scanner(System.in);//Scanner클래스의 객체를 생성
+        System.out.print("국어: ");
+        int korean = scanner.nextInt();//입력받은 내용을 korean에 저장
+        System.out.print("영어: ");
+        int english = scanner.nextInt();
+        System.out.print("수학: ");
+        int math = scanner.nextInt();
+        int avr = ((korean + english + math) / 3)/10;
+        // 평균을 구하고 case를 나누기 위해 10을 나눔
+
+        switch (avr) {
+            case 9:// 몫이 9면 90점 이상이며 avr의 결과가 9와 같으면
+                System.out.println("A");// A 출력
+                break;// 이어서 동작될 수 있기 때문에 break를 아용하여 빠져나감
+            case 8:
+                System.out.println("B");
+                break;
+            case 7:
+                System.out.println("C");
+                break;
+            case 6:
+                System.out.println("D");
+                break;
+            default:// 일치하는 case문이 없을 때
+                System.out.println("불합격입니다.");// 불합격입니다. 출력
+        }
+
+    }
+}
