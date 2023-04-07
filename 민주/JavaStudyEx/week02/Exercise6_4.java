d5b1dd995850c08dd628dfad76cd25154c9977a2
minju0224 <128581270+minju0224@users.noreply.github.com>
2023-04-07 17:38:48 +0900

    Update Exercise6_4.java

--- 민주/JavaStudyEx/week02/Exercise6_4.java
+++ 민주/JavaStudyEx/week02/Exercise6_4.java
@@ -1,6 +1,6 @@
 public class Exercise6_4 {
     public static void main(String[] args){
-        Student s = new Student(); //메소드 선언
+        Student s = new Student(); 
         s.name ="홍길동";
         s.ban=1;
         s.no=1;
